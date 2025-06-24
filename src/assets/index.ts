506dbaefa0166ac08d9ac25f4333103ac10f3bbc
Aayushman Singh <aayushman2702@gmail.com>
2025-06-24 11:54:32 +0530

    hero section svg added

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,3 +2,4 @@
 export { default as childImage } from './child.png';
 export { default as eklavyaLogo } from './eklavya.png';
 export { default as waveImage } from './wave.svg';
+export { default as heroSectionSvg } from './hero-section.svg';
